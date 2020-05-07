7aa04fa56d7ff2013ce909d37783958ade72a24e
Franco Lombardo <f.lombardo69@gmail.com>
2020-05-07 17:31:50 +0200

    Part02 - first commit

---
+++ src/main/kotlin/com/smeup/kotlin/from/scratch/part02/application.kt
@@ -0,0 +1,66 @@
+package com.smeup.kotlin.from.scratch.part02
+
+data class Database(val composers: List<Composer>)
+data class Composer(
+    val name: String,
+    val nation: String,
+    val operas: List<Opera>
+)
+
+data class Opera(val name: String, val yearOfComposition: Int)
+
+fun openDatabase(user: String, password: String): Database? =
+    if ("franco" == user && "secret" == password) {
+        val verdi =
+            Composer("Giuseppe Verdi", "Italy",
+                listOf(Opera("Aida",1871),
+                       Opera("La traviata",1853)))
+        val puccini =
+            Composer("Giacomo Puccini", "Italy",
+                listOf(Opera("Turandot",1926),
+                       Opera("Tosca",1900)))
+        Database((listOf(verdi, puccini)))
+    } else {
+        null
+    }
+
+fun exactName(name: String): (Composer) -> Boolean = { it.name == name }
+
+fun similarName(name: String): (Composer) -> Boolean = { it.name.contains(name, true) }
+
+fun exactMatchToGiuseppeVerdi(composer: Composer): Boolean = composer.name == "Giuseppe Verdi"
+
+val exactMatchToGiacomoPuccini = exactName("Giacomo Puccini")
+
+fun Database.findComposerByName(name: String): Composer? {
+    return findComposerBy(exactName(name))
+}
+
+private fun Database.findComposerBy(predicate: (Composer) -> Boolean) =
+    this.composers.firstOrNull(predicate)
+
+fun Composer.findOperaByYear(year: Int): Opera? =
+    this.operas.firstOrNull { it.yearOfComposition == year }
+
+fun main() {
+    println(
+        openDatabase("franco","secret")
+        ?.findComposerBy(similarName("verdi"))
+        ?.findOperaByYear(1853)
+        ?: "No results"
+    )
+
+    println(
+        openDatabase("franco","secret")
+            ?.findComposerBy(::exactMatchToGiuseppeVerdi)
+            ?.findOperaByYear(1853)
+            ?: "No results"
+    )
+
+    println(
+        openDatabase("franco","secret")
+            ?.findComposerBy(exactMatchToGiacomoPuccini)
+            ?.findOperaByYear(1900)
+            ?: "No results"
+    )
+}
