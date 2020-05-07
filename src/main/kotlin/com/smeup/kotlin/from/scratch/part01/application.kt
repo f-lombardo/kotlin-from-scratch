785cad320237243782338bcd169ecb593df66140
Franco Lombardo <f.lombardo69@gmail.com>
2020-05-07 11:16:07 +0200

    Refactoring

--- src/main/kotlin/com/smeup/kotlin/from/scratch/part01/application.kt
+++ src/main/kotlin/com/smeup/kotlin/from/scratch/part01/application.kt
@@ -1,65 +1,40 @@
 package com.smeup.kotlin.from.scratch.part01
 
 data class Database(val composers: List<Composer>)
-data class Composer(val name: String,
-                    val nation: String,
-                    val operas: List<Opera>)
+data class Composer(
+    val name: String,
+    val nation: String,
+    val operas: List<Opera>
+)
+
 data class Opera(val name: String, val yearOfComposition: Int)
 
 fun openDatabase(user: String, password: String): Database? =
     if ("franco" == user && "secret" == password) {
         val verdi =
-            Composer(
-                "Giuseppe Verdi", "Italy",
-                listOf(
-                    Opera(
-                        "Aida",
-                        1871
-                    ),
-                    Opera(
-                        "La traviata",
-                        1853
-                    )
-                )
-            )
+            Composer("Giuseppe Verdi", "Italy",
+                listOf(Opera("Aida",1871),
+                       Opera("La traviata",1853)))
         val puccini =
-            Composer(
-                "Giacomo Puccini", "Italy",
-                listOf(
-                    Opera(
-                        "Turandot",
-                        1926
-                    ),
-                    Opera(
-                        "Tosca",
-                        1900
-                    )
-                )
-            )
-        Database(
-            (listOf(
-                verdi,
-                puccini
-            ))
-        )
+            Composer("Giacomo Puccini", "Italy",
+                listOf(Opera("Turandot",1926),
+                       Opera("Tosca",1900)))
+        Database((listOf(verdi, puccini)))
     } else {
         null
     }
 
-fun Database.findFirstComposerByNation(nation: String): Composer? =
-    this.composers.firstOrNull { it.nation == nation }
+fun Database.findComposerByName(name: String): Composer? =
+    this.composers.firstOrNull { it.name == name }
 
 fun Composer.findOperaByYear(year: Int): Opera? =
     this.operas.firstOrNull { it.yearOfComposition == year }
 
 fun main() {
     val result =
-        openDatabase(
-            "franco",
-            "secret"
-        )
-        ?.findFirstComposerByNation("Italy")
-        ?.findOperaByYear(1915)
+        openDatabase("franco","secret")
+        ?.findComposerByName("Giuseppe Verdi")
+        ?.findOperaByYear(1853)
         ?: "No results"
     println(result)
 }
