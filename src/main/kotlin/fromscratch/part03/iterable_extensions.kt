e152eb0f102c2628063068a7029f3fb51f751123
Franco Lombardo <f.lombardo69@gmail.com>
2020-06-14 17:31:36 +0200

    Added pluggable context to parallelMap

--- src/main/kotlin/fromscratch/part03/iterable_extensions.kt
+++ src/main/kotlin/fromscratch/part03/iterable_extensions.kt
@@ -1,11 +1,13 @@
 package fromscratch.part03
 
 import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
 suspend fun <A, B> Iterable<A>.concurrentMap(f: suspend (A) -> B?): List<B?> = coroutineScope {
     map { async { f(it) } }.awaitAll()
 }
 
-suspend fun <A, B> Iterable<A>.parallelMap(f: suspend (A) -> B?): List<B?> = withContext(Dispatchers.Default) {
-    map { async { f(it) } }.awaitAll()
-}
+suspend fun <A, B> Iterable<A>.parallelMap(context: CoroutineContext = Dispatchers.Default, f: suspend (A) -> B?): List<B?> =
+    withContext(context) {
+        map { async { f(it) } }.awaitAll()
+    }
