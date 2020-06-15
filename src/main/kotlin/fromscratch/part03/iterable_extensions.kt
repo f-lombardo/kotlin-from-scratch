3a45f50127b854a1a56fad2198ef3ece0c017767
Franco Lombardo <f.lombardo69@gmail.com>
2020-06-15 19:15:15 +0200

    Added link to the source of concurrentMap

--- src/main/kotlin/fromscratch/part03/iterable_extensions.kt
+++ src/main/kotlin/fromscratch/part03/iterable_extensions.kt
@@ -3,6 +3,8 @@ package fromscratch.part03
 import kotlinx.coroutines.*
 import kotlin.coroutines.CoroutineContext
 
+//See https://jivimberg.io/blog/2018/05/04/parallel-map-in-kotlin/
+
 suspend fun <A, B> Iterable<A>.concurrentMap(f: suspend (A) -> B?): List<B?> = coroutineScope {
     map { async { f(it) } }.awaitAll()
 }
